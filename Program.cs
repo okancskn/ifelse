c1a17333b3d94ab9a96ab217afebed1210de9725
Okan Coşkun <okancoskun@Okan-MacBook-Air.local>
2022-08-13 12:03:45 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+        if(time>=6 && time<13)
+        Console.WriteLine("Gunaydin!");
+        else if(time<=18)
+        Console.WriteLine("Iyi Gunler");
+        else
+        Console.WriteLine("Iyi Aksamlar!");
+
+
+        string sonuc = time>=6 && time<13 ? "xGunaydin!" : time <=18 ? "xIyi Günler!" : "xIyi Aksamlar!";
+        Console.WriteLine(sonuc);
+    }
+}
